bfdb940ece5168f07bdaaa5047aebacc73c32507
MountainGod2 <admin@reid.ca>
2026-02-01 20:49:42 -0700

    refactor(manifest): update freeze function to use environment variable for frozen directory

--- frozen/manifest.py
+++ frozen/manifest.py
@@ -3,5 +3,10 @@ Manifest file for frozen modules in MicroPython firmware.
 Modules listed here will be compiled into the firmware.
 """
 
-# Freeze the stripalerts package
-freeze("stripalerts", opt=3)
+import os
+
+# Get the frozen directory from environment variable
+frozen_dir = os.environ.get("FROZEN_DIR", ".")
+
+# Freeze the stripalerts package from the frozen directory
+freeze(os.path.join(frozen_dir, "stripalerts"), opt=3)
