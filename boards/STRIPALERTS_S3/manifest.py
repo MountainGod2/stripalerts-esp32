ea73211763e718b0874a3bd771b870af1900d089
MountainGod2 <admin@reid.ca>
2026-02-04 12:55:38 -0700

    fix(manifest): correct indentation in freeze function for StripAlerts package

--- boards/STRIPALERTS_S3/manifest.py
+++ boards/STRIPALERTS_S3/manifest.py
@@ -5,19 +5,19 @@ include("$(PORT_DIR)/boards/manifest.py")
 
 # Freeze StripAlerts package
 freeze(
-	"$(MPY_DIR)/../../frozen",
-	(
-		"stripalerts/__init__.py",
-		"stripalerts/app.py",
-		"stripalerts/ble.py",
-		"stripalerts/config.py",
-		"stripalerts/constants.py",
-		"stripalerts/events.py",
-		"stripalerts/led.py",
-		"stripalerts/ota.py",
-		"stripalerts/utils.py",
-		"stripalerts/version.py",
-		"stripalerts/wifi.py",
-	),
-	opt=3,
+    "$(MPY_DIR)/../../frozen",
+    (
+        "stripalerts/__init__.py",
+        "stripalerts/app.py",
+        "stripalerts/ble.py",
+        "stripalerts/config.py",
+        "stripalerts/constants.py",
+        "stripalerts/events.py",
+        "stripalerts/led.py",
+        "stripalerts/ota.py",
+        "stripalerts/utils.py",
+        "stripalerts/version.py",
+        "stripalerts/wifi.py",
+    ),
+    opt=3,
 )
