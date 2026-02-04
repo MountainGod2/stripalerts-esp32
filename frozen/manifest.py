049fd0a4a75888d7648113e83eb753e1adf521eb
MountainGod2 <admin@reid.ca>
2026-02-03 17:28:49 -0700

    refactor(manifest): remove obsolete manifest file for frozen modules

--- frozen/manifest.py
+++ frozen/manifest.py
@@ -1,7 +1,12 @@
 """
 Manifest file for frozen modules in MicroPython firmware.
 Modules listed here will be compiled into the firmware.
+
+Note: This file is not used by the build system (tools/cli.py generates its own manifest).
+It's kept here for reference or manual builds.
 """
+include("$(PORT_DIR)/boards/manifest.py")
 
 # Freeze the stripalerts package using recommended package() function
-package("stripalerts", base_path="$(BOARD_DIR)/../../frozen", opt=3)
+# Use absolute path since this is an external module outside the MicroPython tree
+package("stripalerts", base_path="$(MPY_DIR)/../../../frozen", opt=3)
