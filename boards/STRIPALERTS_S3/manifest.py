0c33611724f9fc6cc4cc79e7e3deec52ef93e0de
MountainGod2 <admin@reid.ca>
2026-02-08 17:02:31 -0700

    refactor: update paths for MicroPython submodule

--- boards/STRIPALERTS_S3/manifest.py
+++ boards/STRIPALERTS_S3/manifest.py
@@ -6,20 +6,4 @@ require("aioble")
 require("aiohttp")
 
 # Freeze StripAlerts package
-freeze(
-    "$(MPY_DIR)/../../modules/stripalerts",
-    (
-        "__init__.py",
-        "api.py",
-        "app.py",
-        "ble.py",
-        "config.py",
-        "constants.py",
-        "events.py",
-        "led.py",
-        "utils.py",
-        "version.py",
-        "wifi.py",
-    ),
-    opt=3,
-)
+freeze("$(MPY_DIR)/../../modules/stripalerts", opt=3)
